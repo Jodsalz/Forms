0cb21bc41a6f8c6b7e9e9a38771d61183ee9a5cd
Petar Bogosavljevic <petar.bogosavljevic@siemens.com>
2015-04-08 08:32:39 +0200

    Initial commit

---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetOffice.OutlookApi.Enums;
+
+using NetOffice;
+using Outlook = NetOffice.OutlookApi;
+
+namespace WindowsFormsApplication2
+{
+    
+
+    public partial class Form1 : Form
+    {
+
+        Outlook._NameSpace outlookNS;
+        Outlook.MAPIFolder inboxFolder;
+        Outlook.MAPIFolder ausbuchung;
+        Outlook.MAPIFolder einbuchung;
+        string body = "leer";
+
+        public Form1()
+        {
+            InitializeComponent();
+            NetOffice.OutlookApi.Application outlookApplication = new Outlook.Application();
+            outlookApplication.NewMailExEvent += new Outlook.Application_NewMailExEventHandler(outlook_newmail);
+            outlookNS = outlookApplication.Session;
+
+            inboxFolder = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["Inbox"];
+            einbuchung = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["[Gmail]"].Folders["DevDB"].Folders["Einbuchung"];
+            ausbuchung = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["[Gmail]"].Folders["DevDB"].Folders["Ausbuchung"];
+        }
+
+        private void outlook_newmail(string s)
+        {
+
+            foreach (COMObject item in inboxFolder.Items)
+            {
+                Outlook.MailItem mailItem = item as Outlook.MailItem;
+                if (mailItem != null && mailItem.Subject == "DevDB Einbuchung")
+                {
+                    body = mailItem.Body;
+                   // mailItem.Move(einbuchung);                 
+                }
+                else if (mailItem != null && mailItem.Subject == "DevDB Ausbuchung")
+                {
+                    body = mailItem.Body;
+                   // mailItem.Move(ausbuchung);
+                }
+            }
+            settext(body);
+            
+        }
+
+        private void settext(string s)
+        {
+            label1.Invoke((MethodInvoker)delegate
+            {
+                label1.Text = s;
+            });
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApplication2/Outlok.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetOffice.OutlookApi.Enums;
+
+using NetOffice;
+using Outlook = NetOffice.OutlookApi;
+
+namespace WindowsFormsApplication2
+{
+    public class Outlok
+    {
+        Outlook._NameSpace outlookNS;
+        Outlook.MAPIFolder inboxFolder;
+        Outlook.MAPIFolder ausbuchung;
+        Outlook.MAPIFolder einbuchung;
+        string body;
+
+        public Outlok()
+        {
+
+            NetOffice.OutlookApi.Application outlookApplication = new Outlook.Application();
+            outlookApplication.NewMailExEvent += new Outlook.Application_NewMailExEventHandler(outlook_newmail);
+            outlookNS = outlookApplication.Session;
+
+            inboxFolder = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["Inbox"];
+            einbuchung = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["[Gmail]"].Folders["DevDB"].Folders["Einbuchung"];
+            ausbuchung = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["[Gmail]"].Folders["DevDB"].Folders["Ausbuchung"];
+        }
+
+        private void outlook_newmail(string s)
+        {
+
+          
+            foreach (COMObject item in inboxFolder.Items)
+            {
+                Outlook.MailItem mailItem = item as Outlook.MailItem;
+                if (mailItem != null && mailItem.Subject == "DevDB Einbuchung")
+                {
+                    mailItem.Move(einbuchung);
+                    //body = mailItem.Body;
+                }
+                else if (mailItem != null && mailItem.Subject == "DevDB Ausbuchung")
+                {
+                    mailItem.Move(ausbuchung);
+                    //body = mailItem.Body;
+                }
+            }
+            
+
+        }
+    }
+}
